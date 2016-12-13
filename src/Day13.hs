5806823a306ca6c08bc4395bf441dff763dd71bd
Marek Bernat <marek.bernat@gmail.com>
2016-12-13 23:37:43 +0000

    Solution to day 13.

--- src/Day13.hs
+++ src/Day13.hs
@@ -4,11 +4,14 @@ module Day13 where
 import Data.Bits
 import Data.Dequeue
 import Data.Maybe
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 type Pos = (Int, Int)
 
 input0 = 10
 input1 = 1358
+input2 = 1362
 
 expr :: Pos -> Int
 expr (x, y) = x*x + 3*x + 2*x*y + y + y*y
@@ -26,21 +29,20 @@ neighbours :: Pos -> [Pos]
 neighbours (x,y) = filter valid $ map (\(dx, dy) -> (x + dx, y + dy)) dirs
 
 data State = State
-  { visited :: [Pos]
+  { visited :: Set (Pos, Int)
   , next :: BankersDequeue (Pos, Int)
   }
 
-initialState = State [] (pushBack empty ((1,1), 0))
+initialState = State Set.empty (pushBack empty ((1,1), 0))
 
 walk :: Pos -> State -> State
-walk dest State{..} =
- State visited' next'
+walk dest s@State{..} = if current == dest then s else State visited' next'
   where
     ((current, dist), rest) = fromJust $ popFront next
-    visited' = current : visited
+    visited' = Set.insert (current, dist) visited
     unvisNeigh = map (\p -> (p, dist + 1)) .
-      filter (\p -> not $ p `elem` visited) $ neighbours current
-    next' = if current == foldl pushBack rest unvisNeigh
+      filter (\p -> not $ p `elem` (Set.map fst visited)) $ neighbours current
+    next' = foldl pushBack rest unvisNeigh
 
 dest0 :: (Int, Int)
 dest0 = (7, 4)
