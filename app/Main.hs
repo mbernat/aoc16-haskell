7ff1c7d046409fadd37175f28961647529a885f3
Marek Bernat <marek.bernat@gmail.com>
2016-12-15 00:33:57 +0000

    Partial solution to day 14.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Day1 as D1
 import Day4 as D4
 import Day5 as D5
+import Day14 as D14
 
 main1 :: IO ()
 main1 = do
@@ -14,4 +15,7 @@ main4 = D4.solve2 . D4.load <$> D4.input
 main5 =
     mapM print $ hashes "reyedfim" [10000000..]
 
-main = main5
+main14 =
+    mapM putStrLn $ map (stretch . D14.mkHash input1) $ [0..50000]
+
+main = main14
---
+++ src/Day14.hs
@@ -0,0 +1,66 @@
+module Day14 where
+
+import Data.Hash.MD5
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+
+input0 = "abc"
+input1 = "yjdafjpo"
+
+sHash :: String -> String
+sHash s = md5s $ Str s
+
+mkHash :: String -> Int -> String
+mkHash s n = sHash $ s ++ show n
+
+stretch :: String -> String
+stretch = (!! 2016) . iterate sHash
+
+triple :: String -> Maybe Char
+triple = (head <$>) . find ((>= 3) . length) . group
+
+quint :: String -> [Char]
+quint = map head . filter ((>= 5) . length) . group
+
+type Cache = Map.Map Char [Int]
+
+emptyCache :: Cache
+emptyCache = Map.fromList $ zip (['a'..'f'] ++ ['0'..'9']) (repeat [])
+
+populateCache :: String -> Int -> Cache
+populateCache input up = foldl add emptyCache [0..up]
+  where
+    add m n = foldl (addQuint n) m (quint $ mkHash input n)
+    addQuint n m c = Map.update (Just . (n:)) c m
+
+populateCache2 :: [String] -> Cache
+populateCache2 hashes = foldl add emptyCache (zip [0..] hashes)
+  where
+    add m (n, hash) = foldl (addQuint n) m (quint hash)
+    addQuint n m c = Map.update (Just . (n:)) c m
+
+
+isKey :: String -> Cache -> Int -> Bool
+isKey input cache n = case triple hash of
+    Nothing -> False
+    Just c -> any (\x -> x > n && x <= n + 1000)
+              $ fromJust $ Map.lookup c cache
+  where
+    hash = mkHash input n
+
+isKey2 :: Cache -> (Int, String) -> Bool
+isKey2 cache (n, hash) = case triple hash of
+    Nothing -> False
+    Just c -> any (\x -> x > n && x <= n + 1000)
+              $ fromJust $ Map.lookup c cache
+
+solve input = filter (isKey input cache) [0..q] !! 63
+  where
+    q = 30000
+    cache = populateCache input (q + 1000)
+
+solve2 hashes input = filter (isKey2 cache) (zip [0..] hashes) !! 63
+  where
+    cache = populateCache2 hashes
+
